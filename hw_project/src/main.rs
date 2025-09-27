fcd2a213bf487039151c15556195a8133e40a63d
samgbada06 <150186982+samgbada06@users.noreply.github.com>
2025-09-27 01:34:54 +0000

    intro assignment

--- hw_project/src/main.rs
+++ hw_project/src/main.rs
@@ -29,8 +29,47 @@ fn sum_into(low: i32, high: i32, result: &mut i32) {
     }
 }
 
+fn is_even(n: i32) -> bool {
+    if n % 2 == 0 {
+        true
+    } else {
+        false
+    }
+}
+
+fn check_guess(guess: i32, secret: i32) -> i32 {
+    let secret = 64;
+    if guess > secret {
+        return 1
+    }
+    else if guess < secret {
+        return -1
+    }
+    else {
+        return 0
+    }
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64{
+    let c = (f - 32.0) * 5.0 / 9.0;
+    return c
+}
+fn celsius_to_fahrenheit(c: f64) -> f64{
+    let f = (c * 9.0 / 5.0) + 32.0;
+    return f
+}
+
 fn main() {
-    let mut destination = 0;
-    sum_into(1, 5, &mut destination);
-    println!("The sum is: {}", destination);
-    
+    let fahrFreeze = 32.0;
+    let mut temp = 32.0;
+    let mut i = 0;
+
+    loop{
+        let result = fahrenheit_to_celsius(temp);
+        println!("{}", result);
+        i = 1 + i;
+        temp = temp + 1.0;
+        if i == 5{break;}
+    }
+}
+
